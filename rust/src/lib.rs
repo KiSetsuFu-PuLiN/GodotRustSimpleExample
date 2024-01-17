87a2e247b3c95568a85178cc3d8b9b0e4c2be1c3
KiSetsuFu-PuLiN <kisetsufu-pulin@qq.com>
2024-01-17 17:53:02 +0800

    更新注释

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,8 +2,11 @@ use godot::engine::{ISprite2D, Sprite2D};
 use godot::prelude::*;
 use std::f64::consts::PI;
 
+/// 拓展入口，有此项的动态库才能被 *.gdextension 识别。
+/// `MyExtensition`的名称无关紧要，只要有一个类实现了`ExtensionLibrary`即可。
 struct MyExtensition;
-/// 拓展入口，有此项的dll才能被Godot识别
+/// `#[gdextension]`会将特性实现转化为动态库开放的入口函数`gdext_rust_init`，Godot的*.gdextension需要这个。
+/// 入口函数会自动为下述标有`#[derive(GodotClass)]`的类执行注册和清理工作。
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtensition {}
 
