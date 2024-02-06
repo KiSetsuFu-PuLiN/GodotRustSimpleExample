39921064ea0b13c0ebdc6988004f8ea624eb2faa
KiSetsuFu-PuLiN <kisetsufu-pulin@qq.com>
2024-02-06 15:04:56 +0800

    更新 单例注释

--- godot/new_script.gd
+++ godot/new_script.gd
@@ -9,3 +9,14 @@ func _ready():
 	print(a.b())
 	print(RustResource.b())
 	RustResource.func(a)
+	
+	# 有两种方法访问由 Rust 在 Scene 初始化阶段创建的单例。
+	# 其中一种是直呼其名，如下：
+	RustObjectSingleton
+	# 仅在Scene初始化时创建的单例可以这么获取，因此 GDScript 是做不到创建这种单例的。
+	# 但是这样调用的变量会被视为是常量，无法对其字段和访问器进行赋值。如下：
+	#RustObjectSingleton.texture = preload("res://icon.svg")
+	# rust 和 GDScript 均可以使用 Engine.get_singleton 获取单例。并且这种单例是可变的。
+	var singleton = Engine.get_singleton("RustObjectSingleton") as RustObject
+	singleton.texture = preload("res://icon.svg")
+	add_child(singleton)
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,4 @@
-use godot::engine::{ISprite2D, Sprite2D, IEditorPlugin};
+use godot::engine::{ISprite2D, Sprite2D, IEditorPlugin, Engine};
 use godot::init::EditorRunBehavior;
 use godot::prelude::*;
 
@@ -36,12 +36,30 @@ unsafe impl ExtensionLibrary for MyExtensition {
     /// 自定义扩展初始化时的行为。
     /// 在引擎启动、扩展被初始化时，此函数可能根据引擎的初始化等级而调用多次（ 4 次）， _level 正是当前引擎的初始化等级，扩展的初始化时机比引擎稍晚一些。
     fn on_level_init(_level: InitLevel) {
-        // 默认啥都不干
+        if _level == InitLevel::Scene{
+            // 这里注册了一个单例，使得 GDScript 和 rust 都可以通过`Engine`类来访问一个 RustObject 对象。
+            // 注册单例仅可在`InitLevel::Scene`初始化阶段进行。
+            // 如果发现类似`Engine`这样的类中的字段或方法在 GDScript 中可以正常使用，而在 rust 中提示缺少`&mut self`参数，说明在 rust 里需要额外需要获取单例。
+            // 例如下：`Engine::singleton()`。
+            Engine::singleton().register_singleton(
+                    StringName::from("RustObjectSingleton"),
+                    RustObject::alloc_gd().upcast()
+            );
+        }
     }
     /// 自定义扩展解初始化（析构）时的行为。
     /// 在引擎启动、扩展被初始化时，此函数可能根据引擎的初始化等级而调用多次（ 4 次）， _level 正是当前引擎的初始化等级，扩展的析构时机比引擎稍早一些。
     fn on_level_deinit(_level: InitLevel) {
-        // 默认啥都不干
+        if _level == InitLevel::Scene{
+            // 这里演示了如何取消注册一个单例。
+            let mut engine = Engine::singleton();
+            let singleton_name = "RustObjectSingleton";
+            let singleton = engine
+                    .get_singleton(StringName::from(singleton_name))
+                    .expect(&format!("SingletonNotFuond: {}", singleton_name.clone())[..]);
+            engine.unregister_singleton(StringName::from(singleton_name));
+            singleton.free();
+        }
     }
 }
 
@@ -173,6 +191,7 @@ impl RustResource {
     /// 被`Gd<..>`指针包裹的对象屏蔽了 Godot 的引用带来的干扰，从而被 rust 像普通的智能指针`Rc<..>`一样使用。
     /// 并且同时，`Gd<..>`提供了诸多方法用于兼容 rust 和 GDScript 之间设计理念的差异，比如说 rust 不支持继承。
     /// 可以使用`Gd::<T>::upcast::<TBase>(self)`和`try_cast`来进行类型的派生转换。
+    /// `#[derive(GodotClass)]`修饰的类也实现了`upcast`方法，可以将其转化为父级的的`GD<..>`指针。
     /// 不过获得手动管理类派生功能的代价是，获取指针真正包含的 Godot 对象内容需要使用`Gd::<T>::bind()`或`Gd::<T>::bind_mut()`。
     /// `Gd<..>`默认不为空，如果 GDScript 传了一个 null 进来，则会在运行时发生一个入参为 null 的 panic 错误。
     /// 若要 GDScript 可以传 null 进来，可以使用`Option<Gd<..>>`参数类型。
@@ -190,7 +209,8 @@ impl RustResource {
 /// 使用`#[class(editor_plugin)]`将类识别为插件并进行注册。
 /// 因为由 GDExtension 编写的插件不能像 GDScript 那样手动注册和手动控制启用，没有`plugin.cfg`文件。（C#另说，巨硬给的太多了）。
 /// 因此需要由扩展来主动注册，并且注册即代表启用。
-/// 插件会隐式地被编辑器加载到场景树的根部，可以获取当前正在编辑的场景树的内容。但是必须使用`#[calss(tool)]`才能发挥作用。
+/// 插件会隐式地自动地被编辑器加载到场景树的根部并长久存在，可以获取当前正在编辑的场景树的内容。但是必须使用`#[calss(tool)]`才能发挥作用。
+/// 插件仅会在编辑器模式下起作用，因此即使有`tool`声明也不必区分插件是否是在游戏中运行。但是不可作为场景树节点使用，会在游戏启动时引发无报错的崩溃。
 /// 其他详细请参阅常规 GDScript 中的使用方法。
 #[derive(GodotClass)]
 #[class(init, editor_plugin, base = EditorPlugin, tool)]
@@ -202,12 +222,12 @@ impl IEditorPlugin for RustEditorPlugin{
 
     /// 插件会在引擎启动时被自动加载。
     fn enter_tree(&mut self){
-        godot_print!("插件，启动！")
+        godot_print!("插件，启动！");
     }
 
     /// 这里处理插件的善后工作。
     fn exit_tree(&mut self){
-        godot_print!("插件，关闭！")
+        godot_print!("插件，关闭！");
     }
 
 }
